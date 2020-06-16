c5e71a6ef8c6e775e8dff96676801bd4fd2153c4
dalongtou <493162427@qq.com>
2020-06-16 16:17:41 +0800

    ceshi

--- other/dalongtou_heihei/ceshi/ceshi1.py
+++ other/dalongtou_heihei/ceshi/ceshi1.py
@@ -5,10 +5,10 @@ print('{:-^14}'.format('执行开始'))
 
 for i in range(scale + 1):
     a = '*' * 1
-    b = '--' * (scale - i)
+    b = '-' * (scale - i)
     c = (i / scale) * 100
-    print('\r{:^3.0f}% [{} -> {}]'.format(c , a, b), end='')
+    print('\r{:^3.0f}% [{} -> {}]'.format(c, a, b), end='')
     time.sleep(0.1)
 
 print(' ')
-print('{:-^14}'.format('执行结束'))
+print('{:-^14}'.format('执行结束,2333'))
