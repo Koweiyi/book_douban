e7ccaf82e672b1d60e3a68f1f666f067285ebda9
dalongtou <493162427@qq.com>
2020-06-23 01:04:09 +0800

    update src/douban_music

--- src/douban_music/douban_music/items.py
+++ src/douban_music/douban_music/items.py
@@ -8,7 +8,43 @@
 import scrapy
 
 
-class DoubanMusicItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class MusicTagsItem(scrapy.Item):
+    url = scrapy.Field()
+    pass
+
+
+class MusicTableItem(scrapy.Item):
+    music_url = scrapy.Field()
+    music_id = scrapy.Field()
+    pass
+
+
+class MusicDetailItem(scrapy.Item):
+    music_name = scrapy.Field()
+    music_man = scrapy.Field()
+    music_rename = scrapy.Field()
+    music_sect = scrapy.Field()
+    music_media = scrapy.Field()
+    music_time = scrapy.Field()
+    music_mark = scrapy.Field()
+    music_vote = scrapy.Field()
+    music_star1 = scrapy.Field()
+    music_star2 = scrapy.Field()
+    music_star3 = scrapy.Field()
+    music_star4 = scrapy.Field()
+    music_star5 = scrapy.Field()
+    music_comment1 = scrapy.Field()
+    music_comment2 = scrapy.Field()
+    music_comment3 = scrapy.Field()
+
+    pass
+
+
+class MusicUserItem(scrapy.Item):
+    music_user_site1 = scrapy.Field()
+    music_user_site2 = scrapy.Field()
+    music_user_site3 = scrapy.Field()
+    music_user_time1 = scrapy.Field()
+    music_user_time2 = scrapy.Field()
+    music_user_time3 = scrapy.Field()
     pass
--- src/douban_music/douban_music/pipelines.py
+++ src/douban_music/douban_music/pipelines.py
@@ -4,8 +4,52 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+
+from douban_music import settings
+from douban_music.items import MusicTagsItem, MusicTableItem, MusicDetailItem, MusicUserItem
 
 
 class DoubanMusicPipeline:
+    
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host=settings.MYSQL_HOST,
+            port=settings.MYSQL_PORT,
+            db=settings.MYSQL_DBNAME,
+            user=settings.MYSQL_USER,
+            passwd=settings.MYSQL_PASSWD,
+            charset='utf8',
+            use_unicode=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.connect.cursor()
+    
     def process_item(self, item, spider):
+        if isinstance(item, MusicDetailItem):
+            self.process_musicdetail_item(item)
+            print(item['comment_number'])
+        elif isinstance(item, MusicUserItem):
+            self.process_musicuser_item(item)
+            print(item['comment_number'])
+        elif isinstance(item, MusicTableItem):
+            self.process_musictable_item(item)
+            print(item['comment_number'])
+        elif isinstance(item, MusicTagsItem):
+            self.process_musictags_item(item)
+            print(item['music_name'])
+        
         return item
+
+    def process_musictags_item(self, item):
+        pass
+
+    def process_musictable_item(self, item):
+        pass
+
+    def process_musicuser_item(self, item):
+        pass
+
+    def process_musicdetail_item(self, item):
+        pass
+    
