5213431e0f0389604152f274ea8bf922f4da371b
Koweiyi <1423376854@qq.com>
2020-06-22 20:18:10 +0800

    modiified items.py

--- src/book_douban/book_douban/items.py
+++ src/book_douban/book_douban/items.py
@@ -5,10 +5,33 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class BookDoubanItem(scrapy.Item):
+class BookItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    ID = Field()  # 书本在豆瓣中的subject ID
+    book_name = Field()  # 书本名称
+    book_author = Field()   # 作者
+    publisher = Field()  # 出版社
+    date = Field()  # 出版日期
+    price = Field()  # 价格
+    tags = Field()  # 书本标签
+    intro = Field()  # 书本简介
+    rate = Field()  # 评分
+    rate_pl = Field()  # 评分人数
+    five_star = Field()  # 五星比率
+    for_star = Field()  # 四星比率
+    three_star = Field()  # 三星比率
+    two_star = Field()  # 二星比率
+    one_star = Field()  # 一星比率
+
+
+class CommentItem(Item):
+    ID = Field()  # 评论对应subject ID
+    critic = Field()  # 评论人
+    date = Field()  # 评论时间
+    content = Field()  # 评论内容
+    critic_address = Field()  # 评论人地址
+    star_num = Field()  # 评论星数
