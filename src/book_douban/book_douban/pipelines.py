309e2ef7a0cf8ec7814b71a9bfff64b258c94be4
Koweiyi <1423376854@qq.com>
2020-06-23 19:19:29 +0800

    pipeline insert into db has accomplished.

--- src/book_douban/book_douban/pipelines.py
+++ src/book_douban/book_douban/pipelines.py
@@ -33,10 +33,50 @@ class BookDoubanPipeline:
         return item
 
     def process_book_item(self, item):
+        try:
+            self.cursor.execute('''
+                SELECT * FROM books WHERE ID = %s''', (item['ID'],))
+            book = self.cursor.fetchone()
+            if book is None:
+                self.cursor.execute('''
+                INSERT INTO books(ID, book_name, book_author, publisher, date,
+                price, tags, intro, rate, rate_pl, five_star, four_star, three_star, two_star, one_star) 
+                value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+                                    (item['ID'], item['book_name'], item['book_author'], item['publisher'],
+                                     item['date'], item['price'], item['tags'], item['intro'], item['rate'],
+                                     item['rate_pl'], item['five_star'], item['four_star'], item['three_star'],
+                                     item['two_star'], item['one_star']))
+            self.conn.commit()
+        except Exception as err:
+            print("数据库报错==>错误信息为：" + str(err))
         pass
 
     def process_comment_item(self, item):
+        try:
+            self.cursor.execute('''
+                SELECT * FROM book_comment WHERE ID = %s''', (item['ID'],))
+            comment = self.cursor.fetchone()
+            if comment is None:
+                self.cursor.execute('''
+                INSERT INTO book_comment(ID, critic, date, content, star_num)
+                value(%s, %s, %s, %s, %s)''',
+                                    (item['ID'], item['critic'], item['date'], item['content'], item['star_num']))
+            self.conn.commit()
+        except Exception as err:
+            print("数据库报错==>错误信息为：" + str(err))
         pass
 
     def process_critic_item(self, item):
+        try:
+            self.cursor.execute('''
+            SELECT * FROM douban_user WHERE user_name = %s''', (item['user_name'],))
+            critic = self.cursor.fetchone()
+            if critic is None:
+                self.cursor.execute('''
+                INSERT INTO douban_user(user_name, user_address, join_date)''',
+                                    (item['user_name'], item['user_address'], item['join_date']))
+            self.conn.commit()
+            pass
+        except Exception as err:
+            print("数据库报错==>错误信息为：" + str(err))
         pass
