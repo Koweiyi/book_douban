98d50f83a26aede35dae7694f6adcf2f908ba9c4
clementine <1249295165@qq.com>
2020-06-25 22:58:33 +0800

    line

--- src/movie_douban/movie_douban/movie_douban/items.py
+++ src/movie_douban/movie_douban/movie_douban/items.py
@@ -8,7 +8,31 @@
 import scrapy
 
 
-class MovieDoubanItem(scrapy.Item):
+class MovieItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    dbid = scrapy.Field()
+    title = scrapy.Field()
+    score = scrapy.Field()
+
+
+class MovieDetailItem(scrapy.Item):
+    other_title = scrapy.Field()
+    direct = scrapy.Field()
+    country = scrapy.Field()
+    time = scrapy.Field()
+    type = scrapy.Field()
+    vote_num = scrapy.Field()
+    five_star = scrapy.Field()
+    four_star = scrapy.Field()
+    three_star = scrapy.Field()
+    two_star = scrapy.Field()
+    one_star = scrapy.Field()
+
+
+class MovieCommentItem(scrapy.Item):
+    content = scrapy.Field()
+    comment_time = scrapy.Field()
+    comment_people = scrapy.Field()
+    comment_star = scrapy.Field()
--- src/movie_douban/movie_douban/movie_douban/pipelines.py
+++ src/movie_douban/movie_douban/movie_douban/pipelines.py
@@ -6,7 +6,24 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
 from movie_douban import settings
+from movie_douban.items import MovieItem, MovieDetailItem, MovieCommentItem
+
 
 class MovieDoubanPipeline(object):
+
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host=settings.MYSQL_HOST,
+            port=settings.MYSQL_PORT,
+            db=settings.MYSQL_DBNAME,
+            user=settings.MYSQL_USER,
+            passwd=settings.MYSQL_PASSWD,
+            charset='utf8mb4',
+            use_unicode=True,
+            cursorclass=pymysql.cursors.DictCursor)
+        self.cursor = self.connect.cursor()
+
     def process_item(self, item, spider):
+
+
         return item
