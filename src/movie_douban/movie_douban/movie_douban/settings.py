d3021fbe8f548896240630c4c57877efc6c53ded
clementine <1249295165@qq.com>
2020-06-27 21:29:06 +0800

    final I guess not

--- src/movie_douban/movie_douban/movie_douban/settings.py
+++ src/movie_douban/movie_douban/movie_douban/settings.py
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 2
 
-DEPTH_LIMIT = 4
+#DEPTH_LIMIT = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
