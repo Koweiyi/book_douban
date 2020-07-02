480a3280f335facfe8026232de6ecc19b43cda3a
Koweiyi <1423376854@qq.com>
2020-07-02 22:36:56 +0800

    init project.

---
+++ src/doubanWebApplication/src/test/java/com/iedu/team06/douban/DoubanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iedu.team06.douban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
