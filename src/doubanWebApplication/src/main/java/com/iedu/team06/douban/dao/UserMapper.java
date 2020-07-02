41f6d88245858c5573fcafdd087f5c22e80c9ecb
Koweiyi <1423376854@qq.com>
2020-07-02 23:15:15 +0800

    UserMapper has completed.

--- src/doubanWebApplication/src/main/java/com/iedu/team06/douban/dao/UserMapper.java
+++ src/doubanWebApplication/src/main/java/com/iedu/team06/douban/dao/UserMapper.java
@@ -1,4 +1,31 @@
 package com.iedu.team06.douban.dao;
 
+import com.iedu.team06.douban.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import javax.websocket.server.ServerEndpoint;
+import java.util.List;
+
+@Repository
 public interface UserMapper {
+
+    @Select("select * from user limit 0, 30")
+    public List<User> selectAll();
+
+    @Select("select * from user where uid = #{uid} and pwd = #{pwd}")
+    public User login(@Param("uid") String uid, @Param("pwd") String pwd);
+
+    @Select("select * from user where uid = #{uid}")
+    public User selectByUid(@Param("uid") String uid);
+
+    @Insert("insert into user(uid,pwd,nick_name,state)" +
+            "values (#{user.uid}, #{user.pwd}, #{user.nickName}, #{user.state})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    public int addUser(@Param("user") User user);
+
+
 }
