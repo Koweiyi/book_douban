a790ac5da68834cd517f86fbc6796127c26e9318
clementine <1249295165@qq.com>
2020-07-12 00:53:16 +0800

    增加电影模块

--- src/doubanWebApplication/src/main/java/com/iedu/team06/douban/entity/Movie.java
+++ src/doubanWebApplication/src/main/java/com/iedu/team06/douban/entity/Movie.java
@@ -10,4 +10,17 @@ public class Movie {
     private String score;
     private String other_title;
     private String direct;
+    private String country;
+    private String movie_time;
+    private String movie_type;
+    private String vote_num;
+    private String five_star;
+    private String four_star;
+    private String three_star;
+    private String two_star;
+    private String one_star;
+    private String content;
+    private String comment_time;
+    private String comment_people;
+    private String comment_star;
 }
