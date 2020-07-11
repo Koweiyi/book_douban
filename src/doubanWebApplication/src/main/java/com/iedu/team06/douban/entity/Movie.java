aaf9570dc50c995fbfecb91e77a7224f579ea2fa
clementine <1249295165@qq.com>
2020-07-11 17:48:25 +0800

    增加电影模块

--- src/doubanWebApplication/src/main/java/com/iedu/team06/douban/entity/Movie.java
+++ src/doubanWebApplication/src/main/java/com/iedu/team06/douban/entity/Movie.java
@@ -1,4 +1,13 @@
 package com.iedu.team06.douban.entity;
 
+import lombok.Data;
+
+@Data
 public class Movie {
+
+    private String dbid;
+    private String title;
+    private String score;
+    private String other_title;
+    private String direct;
 }
